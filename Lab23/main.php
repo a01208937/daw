5cdf3409e3d120b49a5b6699e94191682c28c97e
santyarellano <santy.arellano039@gmail.com>
2019-11-11 23:10:32 -0600

    lab empezado (idea)

---
+++ Lab23/main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>LAB 23</title>
+    </head>
+    <body>
+        Lab 23
+        <!-- Desarrollar un login con linkedin -->
+    </body>
+</html>
