80c85cf1882465ce00dd295ef6f27bc5bc07b27f
santyarellano <santy.arellano039@gmail.com>
2019-11-25 19:00:39 -0600

    query 2

--- Lab16/lab.sql
+++ Lab16/lab.sql
@@ -1,2 +1,6 @@
 -- muestra el contenido
 SELECT * FROM Materiales;
+
+-- mostrar el contenido de la tabla Materiales de solo 
+-- aquellos productos cuya clave sea igual a 1000
+SELECT * FROM materiales WHERE clave=1000;
