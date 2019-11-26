294c458fcd72657895a3321206087304f32ed8b5
santyarellano <santy.arellano039@gmail.com>
2019-11-26 09:59:17 -0600

    preguntas

--- Lab22/lab.sql
+++ Lab22/lab.sql
@@ -42,3 +42,21 @@ AS
 GO
 
 EXECUTE eliminaMaterial 5000
+
+--4
+IF EXISTS (SELECT name FROM sysobjects
+            WHERE name = 'queryMaterial' AND type = 'P')
+    DROP PROCEDURE queryMaterial
+GO
+
+CREATE PROCEDURE queryMaterial
+    @udescripcion VARCHAR(50),
+    @ucosto NUMERIC(8,2)
+
+AS
+    SELECT * FROM Materiales WHERE descripcion
+    LIKE '%'+@udescripcion+'%' AND costo > @ucosto
+GO
+
+EXECUTE queryMaterial 'Lad',20
+
