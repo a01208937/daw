8af3e634934c412febe01a274fdf7fcca4d9038b
santyarellano <santy.arellano039@gmail.com>
2019-11-25 19:04:47 -0600

    some advances

--- Lab16/lab.sql
+++ Lab16/lab.sql
@@ -1,6 +1,31 @@
--- muestra el contenido
 SELECT * FROM Materiales;
 
--- mostrar el contenido de la tabla Materiales de solo 
--- aquellos productos cuya clave sea igual a 1000
 SELECT * FROM materiales WHERE clave=1000;
+
+select clave,rfc,fecha from entregan;
+
+select * from entregan,proyectos 
+where entregan.numero < = proyectos.numero
+
+(select * from entregan where clave=1400) 
+union 
+(select * from entregan where clave=1300)
+
+select * from materiales,entregan
+where materiales.clave = entregan.clave
+
+select * from entregan where clave=1450 AND clave=1300
+
+(select clave from entregan where numero=5001)
+intersect
+(select clave from entregan where numero=5018)
+
+(select * from entregan)
+minus
+(select * from entregan where clave=1000)
+
+(select * from entregan)
+EXCEPT
+(select * from entregan where clave=1000)
+
+select * from entregan,materiales
