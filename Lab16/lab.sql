9d91df285a67a561fe5a291070a90b81bb82f9e1
santyarellano <santy.arellano039@gmail.com>
2019-11-25 19:10:25 -0600

    some advances

--- Lab16/lab.sql
+++ Lab16/lab.sql
@@ -154,4 +154,54 @@ SELECT TOP 1 E.Clave, SUM(Cantidad) as 'Total'
 FROM Entregan as E
 WHERE (Fecha Between '01-JAN-2001' AND '31-DEC-2001')
 GROUP BY E.Clave
-ORDER BY Total DESC
+ORDER BY Total DESC;
+
+select * from top2001;
+
+CREATE VIEW patronUB(descripcion) AS
+SELECT descripcion
+FROM Materiales AS M
+WHERE descripcion LIKE '%ub%';
+
+select * from patronUB;
+
+CREATE VIEW denominacionySuma(denominacion ,sumaTotal) AS
+SELECT P.Denominacion, SUM(E.Cantidad*(M.Costo*(PorcentajeImpuesto/100)+1)) as "Total"
+FROM Entregan AS E, Proyectos AS P, Materiales AS M
+WHERE P.Numero=E.Numero AND M.Clave=E.Clave
+GROUP BY P.Denominacion;
+
+select * from denominacionySuma;
+
+CREATE VIEW televisaCoahuila(denominacion , RFC, RazonSocial) AS
+     (  SELECT DISTINCT P.Denominacion, PR.RFC, PR.RazonSocial
+     FROM Entregan AS E, Proyectos AS P, Proveedores as PR
+     WHERE P.Numero=E.Numero AND PR.RFC=E.RFC AND P.Denominacion LIKE 'Tel%' AND PR.RFC NOT IN (SELECT PR.RFC FROM Entregan AS E, Proyectos AS P, Proveedores as PR
+     WHERE P.Numero=E.Numero AND PR.RFC=E.RFC AND P.Denominacion LIKE 'Edu%' ))
+
+select * from televisaCoahuila;
+
+SELECT DISTINCT P.Denominacion, PR.RFC, PR.RazonSocial
+     FROM Entregan AS E, Proyectos AS P, Proveedores as PR
+     WHERE P.Numero=E.Numero AND PR.RFC=E.RFC AND P.Denominacion LIKE 'Tel%' AND PR.RFC NOT IN (SELECT PR.RFC FROM Entregan AS E, Proyectos AS P, Proveedores as PR
+     WHERE P.Numero=E.Numero AND PR.RFC=E.RFC AND P.Denominacion LIKE 'Edu%' )
+
+CREATE VIEW costoMatTelevisaCoahuila(costo ,descripcion) AS
+     SELECT M.Costo, M.Descripcion
+     FROM Entregan AS E, Proyectos AS P, Materiales AS M, Proveedores AS PR
+     WHERE P.Numero=E.Numero AND PR.RFC=E.RFC AND M.Clave=E.Clave AND M.Clave=E.Clave AND P.Denominacion LIKE 'Tel%' AND PR.RFC IN(
+       SELECT PR.RFC
+       FROM Entregan AS E, Proyectos AS P, Proveedores as PR
+       WHERE P.Numero=E.Numero AND PR.RFC=E.RFC AND P.Denominacion LIKE 'Edu%'
+       )
+
+select * from costoMatTelevisaCoahuila
+
+CREATE VIEW totalmateriales(nombre,proyecto, cantidadEntrega,costoTotal) AS
+SELECT M.Descripcion, P.Denominacion, SUM(E.Cantidad) as 'cantidadEntrega', SUM(E.Cantidad*(M.Costo*(PorcentajeImpuesto/100)+1)) as "costoTotal"
+FROM Entregan AS E, Materiales AS M,  Proyectos AS P
+WHERE M.Clave=E.Clave AND P.Numero=E.Numero
+GROUP BY M.Descripcion, P.Denominacion
+
+
+select * from totalmateriales;
